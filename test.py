4f835169b9814cea4cb0abf846bc68ebdef42fd9
Jinzhong Xu <xujinzh@JinzhongdeMacBook-Pro.local>
2018-04-20 22:10:54 +0800

    do not display the original data image

--- test.py
+++ test.py
@@ -13,15 +13,15 @@ import matplotlib.pyplot as plt
 import time
 from entropy import filtration_entropy
 
-plt.rcParams['font.sans-serif'] = ['SimHei']        # 用来正常显示中文标签
-plt.rcParams['axes.unicode_minus'] = False        # 用来正常显示负号
+# plt.rcParams['font.sans-serif'] = ['SimHei']        # 用来正常显示中文标签
+# plt.rcParams['axes.unicode_minus'] = False        # 用来正常显示负号
 
-circle100 = pd.read_csv('circle50r2.csv')
+circle100 = pd.read_csv('c100.csv')
 circle100 = circle100[['x1', 'x2']]
-print(circle100.head())
-plt.plot(circle100['x1'], circle100['x2'], 'k.')
-plt.title(u'带有高斯噪声的圆')
-plt.show()
+# print(circle100.head())
+# plt.plot(circle100['x1'], circle100['x2'], 'k.')
+# plt.title(u'带有高斯噪声的圆')
+# plt.show()
 
 tic = time.time()
 filtration_entropy(circle100, 0.0)
